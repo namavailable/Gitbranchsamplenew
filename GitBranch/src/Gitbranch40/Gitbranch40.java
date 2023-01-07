a56c4e98dec83c3bec23a4423c298773610f1a8e
LinYenCheng <tmpss91015@gmail.com>
2023-01-07 14:29:13 +0800

    add math

--- GitBranch/src/Gitbranch40/Gitbranch40.java
+++ GitBranch/src/Gitbranch40/Gitbranch40.java
@@ -4,7 +4,15 @@ public class Gitbranch40 {
 
 	public static void main(String[] args) {
 		System.out.println("D¯à¦Ó");
-
+		System.out.println("version 2");
+		Math cMath=new Math();
+		System.out.println(cMath.add(10, 20));
+		
 	}
 
 }
+class Math{
+	 int add(int a, int b) {
+	  return a + b;
+	 }
+	}
